4e85d17bad429c876aae984303bbc93ad830e75e
Valtteri Seuranen <K1967@Labranet.jamk.fi>
2017-10-12 12:09:15 +0300

    manage connections method, jsdoc comments

--- BluetoothProject/app/src/main/java/com/ddcv/jamk/dev/bluetooth/myapplication/MainActivity.java
+++ BluetoothProject/app/src/main/java/com/ddcv/jamk/dev/bluetooth/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Debug;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -37,11 +38,28 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.logging.Handler;
 
 public class MainActivity extends Activity {
 
+
+    /**
+     * @member  ProgressBar                 Shown when devices are searched
+     * @member  RecyclerView                List of found devices
+     * @member  RecyclerView.Adapter        Manages devices shown in the list
+     * @member  RecyclerView.LayoutManager  Manager the overlay of the list
+     *
+     * @member  ENABLE_BLUETOOTH_CODE       Tag for permissions
+     * @member  ALLOW_BLUETOOTH_CODE        Tag for permissions
+     *
+     * @member  foundDevices                Array of found BluetoothDevices
+     *
+     * @member  mBluetoothAdapter           Adapter that handles phones bluetooth connection
+     */
 
     private ProgressBar spinningCircle;
 
@@ -56,73 +74,10 @@ public class MainActivity extends Activity {
     //private ArrayList<BluetoothDevice> foundDevices;
     private BluetoothAdapter mBluetoothAdapter;
 
-
-
-
-    //when bluetooth service has found new device
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver()
-    {
-        @Override
-        public void onReceive(Context context, Intent intent)
-        {
-
-            String action = intent.getAction();
-            mBluetoothAdapter.cancelDiscovery();
-
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                //bluetooth device is found.
-
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                foundDevices.put(device.getAddress(), device);
-
-
-
-                //hide progressbar
-                spinningCircle.setVisibility(View.INVISIBLE);
-
-                updateDeviceList();
-
-                //ConnectThread conThread = new ConnectThread(device);
-
-                /*
-                Discover me
-
-                AcceptThread thread = new AcceptThread();
-                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,0);
-                startActivity(discoverableIntent);
-
-                thread.run();
-                */
-
-                Log.i("what", "complete");
-            }
-        }
-    };
-
-    //update shown list of detected bluetooth devices
-    private void updateDeviceList(){
-
-        deviceList.setAdapter(mAdapter);
-
-        if(foundDevices == null)
-        {
-            Log.e("Bluetooth", "updating devicelist, but the founddevices array is null");
-            return;
-        }
-        else if(foundDevices.size() <= 0)
-        {
-            Log.e("Bluetooth", "No devices available");
-            return;
-        }
-        else
-        {
-            DeviceAdapter deviceListAdapter = new DeviceAdapter(foundDevices);
-            deviceListAdapter.mCallback.selectDevice(); //https://developer.android.com/training/basics/fragments/communicating.html
-            deviceList.setAdapter(deviceListAdapter);
-        }
-    }
-
+    /**
+     * App is created
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -187,23 +142,86 @@ public class MainActivity extends Activity {
 
     }
 
-    //set to register found Bluetooth requests
+    /**
+     * When app is closed, remove registers
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+
+    /**
+     * @callback mReceiver
+     * When new bluetooth device is detected, while we are scanning for new devices
+     */
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+
+            String action = intent.getAction();
+            mBluetoothAdapter.cancelDiscovery();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device is found.
+
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                foundDevices.put(device.getAddress(), device);
+
+
+
+                //hide progressbar
+                spinningCircle.setVisibility(View.INVISIBLE);
+
+                updateDeviceList();
+
+                //ConnectThread conThread = new ConnectThread(device);
+
+                /*
+                Discover me
+
+                AcceptThread thread = new AcceptThread();
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,0);
+                startActivity(discoverableIntent);
+
+                thread.run();
+                */
+
+                Log.i("Bluetooth_brodcast", "Bluetooth device detected!");
+            }
+        }
+    };
+
+
+    /**
+     * set to register found Bluetooth requests
+     */
     private void bluetoothIsEnabled(){
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter);
         FindNewDevices();
     }
 
-    //Check if device is already known
-    private void IsDevicePaired() {
+    /**
+     * Check if selected device has been paired with
+     * @param device, device under inception
+     */
+    private void IsDevicePaired(BluetoothDevice device) {
     }
 
+    /**
+     * Read already paired BluetoothDevices from phone's bluetooth adapter
+     */
     private void getPairedDevices(){
 
     }
 
-
-    //find previously unkown devices
+    /**
+     * Check permissions for detecting bluetooth devices and start scanning
+     */
     private void FindNewDevices() {
 
         //check if app has permission to use Bluetooth (X >= Android 6.0)
@@ -215,8 +233,40 @@ public class MainActivity extends Activity {
 
         int granted = PermissionChecker.PERMISSION_GRANTED;
 
+        /**
+         * If some of the necessary permissions are not granted, App will ask the user for them...
+         */
         if(selfPermission != granted || adminPermission != granted || fineLocation != granted || coarseLocation != granted)
         {
+            /**
+             * Show alert dialog informing what permissions are required for this app to work
+             */
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Permissions have not been granted");
+            alert.setMessage("Permissions for coarse/fine location and Bluetooth required");
+            alert.setCancelable(false);
+
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    FindNewDevices(); //rerun current method
+                }
+            });
+
+            alert.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //close application
+                    System.exit(0);
+                }
+            });
+
+            AlertDialog dialog = alert.create();
+            dialog.show();
+
+            /**
+             * Send the required permissions permissionRequest
+             */
             ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.BLUETOOTH,
                     Manifest.permission.BLUETOOTH_ADMIN,
@@ -226,7 +276,9 @@ public class MainActivity extends Activity {
             return;
         }
 
-        //start searching devices
+        /**
+         * start searching devices
+         */
         if(mBluetoothAdapter.getState() == BluetoothAdapter.STATE_ON)
         {
             spinningCircle.setVisibility(View.VISIBLE);
@@ -239,6 +291,37 @@ public class MainActivity extends Activity {
 
     }
 
+    /**
+     * Update shown recyclerView when new bluetooth devices has been found
+     */
+    private void updateDeviceList(){
+
+        deviceList.setAdapter(mAdapter);
+
+        if(foundDevices == null)
+        {
+            Log.e("Bluetooth", "updating devicelist, but the founddevices array is null");
+            return;
+        }
+        else if(foundDevices.size() <= 0)
+        {
+            Log.e("Bluetooth", "No devices available");
+            return;
+        }
+        else
+        {
+            DeviceAdapter deviceListAdapter = new DeviceAdapter(foundDevices);
+            deviceListAdapter.mCallback.selectDevice(); //https://developer.android.com/training/basics/fragments/communicating.html
+            deviceList.setAdapter(deviceListAdapter);
+        }
+    }
+
+    /**
+     * If permissions were granted
+     * @param requestCode   Code to indetify send request
+     * @param permissions      what permissions were asked
+     * @param grantResults      what was the response from phone
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
@@ -250,6 +333,12 @@ public class MainActivity extends Activity {
         }
     }
 
+    /**
+     * Was bluetooth enabled?
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -276,13 +365,9 @@ public class MainActivity extends Activity {
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mReceiver);
-    }
-
-    //receive Bluetooth connections from other devices
+    /**
+     * @class   AcceptThread     receive Bluetooth connections from other devices.
+     */
     public class AcceptThread extends Thread {
 
         private final BluetoothServerSocket mServerSocket;
@@ -333,7 +418,9 @@ public class MainActivity extends Activity {
 
     }
 
-    //connect to other found bluetooth devices
+    /**
+     * @class   ConnectThread     Connect to other bluetooth devices
+     */
     public class ConnectThread extends Thread {
 
         private final BluetoothSocket mSocket;
@@ -389,4 +476,111 @@ public class MainActivity extends Activity {
             }
         }
     }
+
+    /**
+     * @class   ConnectedThread     Manage connection between two devices
+     */
+    private class ConnectedThread extends Thread{
+
+        /**
+         * @member  mSocket         Contains target bluetoothsocket
+         * @member  mInputStream    The data received from other device
+         * @member  mOutputStream   The data sent to other device
+         * @member  mBuffer         Temporary variable contains the data
+         * @member  mHandler        Handle data reading
+         */
+        private final BluetoothSocket mSocket;
+        private final InputStream mInputStream;
+        private final OutputStream mOutputStream;
+        private byte[] mBuffer;
+        private android.os.Handler mHandler;
+
+        /**
+         * @function     ConnectThread  Constructor
+         * @param       "socket"        Socket assigned to communicate with specified device
+         */
+        public ConnectedThread(BluetoothSocket socket) {
+            mSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException error) {
+                Log.e("Bluetooth_connector", "ConnectedThread InputStream failed", error);
+            }
+
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException error) {
+                Log.e("Bluetooth_connector", "ConnectedThread OutputStream failed", error);
+            }
+
+            mInputStream = tmpIn;
+            mOutputStream = tmpOut;
+        }
+
+        /**
+         * Get data from remote device
+         */
+        public void run(){
+            mBuffer = new byte[1024];
+            int numBytes;
+
+            while(true){
+                try {
+                    numBytes = mInputStream.read(mBuffer);
+
+                    /**
+                     * @param {int}     arg1    What is received
+                     * @param {int}     arg2
+                     * @param {int}     arg3
+                     * @param {byte[]}  arg4    The message
+                     */
+                    Message mMessage = mHandler.obtainMessage(0, numBytes, -1, mBuffer);
+
+                    mMessage.sendToTarget();
+
+                } catch (IOException e){
+                    Log.e("Bluetooth_error", "Error with reading data", e);
+                    break;
+                }
+            }
+        }
+
+        /**
+         * @function write  Send data "bytes" to remote device
+         * @param bytes The data to be send
+         */
+        public void write(byte[] bytes){
+            try {
+                mOutputStream.write(bytes);
+
+                Message writtenMsg = mHandler.obtainMessage(1, -1, -1, mBuffer);
+                writtenMsg.sendToTarget();
+
+            }catch (IOException e){
+                Log.e("Bluetooth_message", "Error in writing to remote device", e);
+
+                //send failure msg to activity
+                Message writtenMsg = mHandler.obtainMessage(2);
+                Bundle bundle = new Bundle();
+                bundle.putString("toast", "Could not send data!");
+                writtenMsg.setData(bundle);
+                mHandler.sendMessage(writtenMsg);
+            }
+        }
+
+        /**
+         * @function cancel Close the thread
+         */
+        public void cancel(){
+            try {
+                mSocket.close();
+            } catch (IOException e){
+                Log.e("Bluetooth_connector", "Closing mSocket failed", e);
+            }
+        }
+    }
+
 }
