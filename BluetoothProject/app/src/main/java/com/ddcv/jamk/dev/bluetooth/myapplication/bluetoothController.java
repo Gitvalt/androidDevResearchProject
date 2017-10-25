27b9b008bbf774ab59b373ce8fd1ed20d3c86948
Valtteri Seuranen <K1967@Labranet.jamk.fi>
2017-10-25 13:11:01 +0300

    no thanks

---
+++ BluetoothProject/app/src/main/java/com/ddcv/jamk/dev/bluetooth/myapplication/bluetoothController.java
@@ -0,0 +1,12 @@
+package com.ddcv.jamk.dev.bluetooth.myapplication;
+
+/**
+ * Created by K1967 on 25.10.2017.
+ */
+
+public class bluetoothController {
+
+    public bluetoothController() {
+    }
+
+}
